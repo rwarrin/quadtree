186729d1d57d2d6232a765c052383e0a0b137c8d
Rick Warrington <phial1020@gmail.com>
2015-05-29 19:27:30 -0600

    Add Node functionality

--- include/node.h
+++ include/node.h
@@ -1,6 +1,18 @@
 #ifndef NODE_H
 #define NODE_H
 
+struct node
+{
+    double x;
+    double y;
+    int id;
+};
 
+struct node** node_new_array(int size);
+void node_free_array(struct node **nodes, int node_count);
+struct node* node_insert(struct node **nodes, int index, double x, double y, int id);
+struct node* node_remove(struct node **nodes, int index, int *count);
+void node_shift_nodes(struct node **nodes, int index, int count);
+struct node* node_find_by_id(struct node **nodes, int size, int id);
 
 #endif  // NODE_H
--- src/node.c
+++ src/node.c
@@ -4,3 +4,79 @@
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
+
+struct node** node_new_array(int size)
+{
+    struct node **nodes = malloc(sizeof(struct node*) * size);
+    assert(nodes != NULL);
+
+    return nodes;
+}
+
+void node_free_array(struct node **nodes, int node_count)
+{
+    assert(nodes != NULL);
+
+    for(int i = 0; i < node_count; i++)
+    {
+        free(nodes[i]);
+    }
+
+    free(nodes);
+}
+
+struct node* node_insert(struct node **nodes, int index, double x, double y, int id)
+{
+    assert(nodes != NULL);
+
+    struct node *node = malloc(sizeof(struct node));
+    node->x = x;
+    node->y = y;
+    node->id = id;
+
+    nodes[index] = node;
+    return node;
+}
+
+struct node* node_remove(struct node **nodes, int index, int *count)
+{
+    assert(nodes != NULL);
+    struct node *ret_node = nodes[index];
+
+    node_shift_nodes(nodes, index, *count);
+    *count = *count - 1;
+
+    return ret_node;
+}
+
+void node_shift_nodes(struct node **nodes, int index, int count)
+{
+    assert(nodes != NULL);
+
+    for(int i = index; i < count - 1; i++)
+    {
+        nodes[i] = nodes[i + 1];
+    }
+}
+
+struct node* node_find_by_id(struct node **nodes, int size, int id)
+{
+    assert(nodes != NULL);
+
+    struct node *finder = NULL;
+
+    for(int i = 0; i < size; i++)
+    {
+        if(nodes[i] == NULL)
+        {
+            break;
+        }
+        if(id == nodes[i]->id)
+        {
+            finder = NULL;
+            break;
+        }
+    }
+
+    return finder;
+}
